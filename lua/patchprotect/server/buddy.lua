b39bd38ca53765c7882c86da7338cb1a766d50b1
storm37000 <6331913+storm37000@users.noreply.github.com>
2020-02-25 21:20:42 -0500

    bug fixes

--- lua/patchprotect/server/buddy.lua
+++ lua/patchprotect/server/buddy.lua
@@ -1,10 +1,9 @@
 -- SEND BUDDIES TO CLIENT
-local function sv_PProtect_sendbuddies(ply, bud, sendto)
+local function sv_PProtect_sendbuddies(ply, sendto)
   if ply == nil then return end
   net.Start('pprotect_send_buddies')
    net.WriteEntity(ply)
-   if ply.Buddies == nil then ply.Buddies = {} end
-   net.WriteTable(ply.Buddies[bud] or {})
+   net.WriteTable(ply.Buddies or {})
   if sendto == nil then
     net.Broadcast()
   else
@@ -13,7 +12,7 @@ local function sv_PProtect_sendbuddies(ply, bud, sendto)
 end
 
 net.Receive('pprotect_request_buddies', function(len, ply)
-	sv_PProtect_sendbuddies(player.GetBySteamID(net.ReadString()), ply:SteamID(), ply)
+	sv_PProtect_sendbuddies(player.GetBySteamID(net.ReadString()), ply)
 end)
 
 -- NOTIFICATION/MODIFICATION
@@ -24,7 +23,7 @@ net.Receive('pprotect_info_buddy', function(len, ply)
   if ply.Buddies == nil then ply.Buddies = {} end
   if tbl.bud and ply.Buddies[sid] and tbl.bud == ply.Buddies[sid].bud then
 	ply.Buddies[sid] = tbl
-	sv_PProtect_sendbuddies(ply,sid)
+	sv_PProtect_sendbuddies(ply)
     return 
   end
   if tbl.bud then
@@ -34,5 +33,5 @@ net.Receive('pprotect_info_buddy', function(len, ply)
     ply.Buddies[sid] = nil
     sv_PProtect.Notify(bud, ply:Nick() .. ' removed you as a buddy.', 'normal')
   end
-  sv_PProtect_sendbuddies(ply,sid)
+  sv_PProtect_sendbuddies(ply)
 end)
