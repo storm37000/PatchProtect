92742b8ac1d23b7c79a60e8ef1f4e0e99cccfe54
storm37000 <6331913+storm37000@users.noreply.github.com>
2022-05-15 22:26:21 -0400

    fix rare error with poorly coded addons.  I'm looking at you no_collide_multi

--- lua/patchprotect/server/propprotection.lua
+++ lua/patchprotect/server/propprotection.lua
@@ -39,7 +39,7 @@ function undo.AddEntity(ent)
     undo.Create("something")
   end
   ue(ent)
-  if ent == nil then return end
+  if not IsValid( ent ) then return end
   table.insert(en.e, ent)
   if not ent.ppowner and IsValid(en.o) then
     ent:CPPISetOwner(en.o)
